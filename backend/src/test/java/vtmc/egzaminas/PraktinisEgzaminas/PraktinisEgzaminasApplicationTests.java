86032e8570112e85031972177cf67461428b7a2b
AivarasP <aivaraspuodziunas9@gmail.com>
2022-06-27 23:21:16 +0300

    Uploading backend/frontend files

---
+++ backend/src/test/java/vtmc/egzaminas/PraktinisEgzaminas/PraktinisEgzaminasApplicationTests.java
@@ -0,0 +1,13 @@
+package vtmc.egzaminas.PraktinisEgzaminas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktinisEgzaminasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
